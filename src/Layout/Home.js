15cd214c1dda6aed22697557e70a338964c37263
Micah Barwick <mbarwick@indeed.com>
2024-01-11 13:01:18 -0800

    initial commit from another project.

---
+++ src/Layout/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+
+    // TODO: Show a list of decks with options to create, study, view, or delete a deck.
+    return (
+        <div>
+            <h2>This be the Home Page</h2>
+        </div>
+    )
+}
+
+export default Home;
